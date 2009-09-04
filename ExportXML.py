8e129c9e329d6d5b2e2da78ef38d294f20b163b7
Anthony Tuininga <anthony.tuininga@gmail.com>
2009-09-04 20:32:29 +0000

    Added option --sort-by to allow the result set to be sorted before exporting; added support for specifying a query rather than simply a table name to allow for additional customization as required; removed the rownum attribute from the rows since it serves no useful purpose; use cx_Logging rather than printing directly to sys.stderr. Thanks to Micah Friesen for the initial patch.

--- ExportXML.py
+++ ExportXML.py
@@ -1,5 +1,6 @@
 """Export data from a table in an Oracle database to an XML file."""
 
+import cx_Logging
 import cx_LoggingOptions
 import cx_OptionParser
 import cx_Oracle
@@ -17,21 +18,31 @@ parser.AddOption(Options.ARRAY_SIZE)
 parser.AddOption(Options.REPORT_POINT)
 parser.AddOption(Options.SET_ROLE)
 parser.AddOption(Options.DATE_FORMAT)
+parser.AddOption("--sort-by", metavar = "STR",
+        help = "append an order by clause with this value to the query")
 cx_LoggingOptions.AddOptions(parser)
 parser.AddArgument("fileName", required = True,
         help = "the name of the file to which to write the data or "
                "'-' to write the data to stdout")
-parser.AddArgument("tableName", required = True,
-        help = "the name of the table from which to export the data")
+parser.AddArgument("source", required = True,
+        help = "the name of the table from which to export all of the data " \
+               "or the select statement which should be executed to " \
+               "determine the data to export")
 options = parser.Parse()
 cx_LoggingOptions.ProcessOptions(options)
 
-# connect to the database
+# connect to the database and execute the query
 connection = cx_OracleUtils.Connect(options.schema, options.setRole)
 cursor = connection.cursor()
 if options.arraySize is not None:
     cursor.arraysize = options.arraySize
-cursor.execute("select * from %s" % options.tableName)
+if options.source.lower().startswith("select "):
+    query = options.source
+else:
+    query = "select * from %s" % options.source
+if options.sortBy:
+    query += " order by %s" % options.sortBy
+cursor.execute(query)
 names = [item[0] for item in cursor.description]
 
 # write the data to the XML file
@@ -42,7 +53,7 @@ else:
 writer = cx_XML.Writer(outputFile, numSpaces = 4)
 writer.StartTag("ROWSET")
 for row in cursor:
-    writer.StartTag("ROW", num = cursor.rowcount)
+    writer.StartTag("ROW")
     for name, value in zip(names, row):
         if value is None:
             continue
@@ -56,10 +67,10 @@ for row in cursor:
     writer.EndTag()
     if options.reportPoint is not None \
             and cursor.rowcount % options.reportPoint == 0:
-        print >> sys.stderr, cursor.rowcount, "rows exported."
+        cx_Logging.Trace("%s rows exported.", cursor.rowcount)
 writer.EndTag()
 
 if options.reportPoint is None or cursor.rowcount % options.reportPoint != 0:
-    print >> sys.stderr, cursor.rowcount, "rows exported."
-print >> sys.stderr, "Done."
+    cx_Logging.Trace("%s rows exported.", cursor.rowcount)
+cx_Logging.Trace("Done.")
 
